4593ab4acc7f800a731ef4124b8efad98fa64b52
Sam Hoang Van <samhv.ict@gmail.com>
2015-07-14 20:38:42 +0700

    Rea: Change return type to readable in sentry

--- parser.py
+++ parser.py
@@ -6,32 +6,32 @@ import raven
 
 LOG_TYPES = {
     'HAPROXY': [
-        '{ip}:{port} ',
-        '[{times}] ',
+        '{ip}:{port:d} ',
+        '[{times:get_time_haproxy}] ',
         '{frontend_name_transport} ',
         '{backend_name}/{server_name} ',
-        '{tq}/{tw}/{tc}/{tr}/{tt} ',
-        '{status_code} ',
-        '{bytes_sent} ',
+        '{tq:d}/{tw:d}/{tc:d}/{tr:d}/{tt:d} ',
+        '{status_code:d} ',
+        '{bytes_sent:d} ',
         '{captured_request_cookie} ',
         '{captured_response_cookie} ',
         '{country} ',
-        '{actconn}/{feconn}/{beconn}/{srv_conn}/{retries} ',
-        '{srv_queue}/{backend_queue} ',
+        '{actconn:d}/{feconn:d}/{beconn:d}/{srv_conn:d}/{retries:d} ',
+        '{srv_queue:d}/{backend_queue:d} ',
         '{{{other}}} "{http_request}"',
     ],
 
     'LIGHTTPD': [
         '{ip} ',
-        '[{time}] ',
+        '[{time:th}] ',
         '"{http_request}" ',
-        '{status_code} ',
-        '{byte_sent} ',
+        '{status_code:d} ',
+        '{byte_sent:d} ',
         '"{referer}" ',
         '"{user_agent}" ',
-        '{byte_in} ',
-        '{byte_out} ',
-        '{time_used} ',
+        '{byte_in:d} ',
+        '{byte_out:d} ',
+        '{time_used:d} ',
         '{request_hostname}',
     ],
 
@@ -39,56 +39,80 @@ LOG_TYPES = {
     'NGINX': [
         '{ip} ',
         '{http_x_forwarded_for} ',
-        '[{time}] ',
+        '[{time:ti}] ',
         '{http_host} ',
         '"{http_request}" ',
-        '{status_code} ',
-        '{bytes_sent} ',
+        '{status_code:d} ',
+        '{bytes_sent:d} ',
         '"{http_referer}" ',
         '{http_user_agentt}',
     ],
 
     'ATS': [
-        '{time} RESPONSE: sent ',
+        '{time:get_time_ats} RESPONSE: sent ',
         '{ip} status ',
-        '{status_code} ',
+        '{status_code:d} ',
         '({accelerator}) for ',
         "'{http_referer}'",
     ],
 
     'HAPROXY2': [
-        '{ip}:{port} ',
-        '[{times}] ',
+        '{ip}:{port:d} ',
+        '[{times:get_time_haproxy}] ',
         '{frontend_name_transport} ',
         '{backend_name}/{server_name} ',
-        '{tq}/{tw}/{tc}/{tr}/{tt} ',
-        '{status_code} ',
-        '{bytes_sent} ',
+        '{tq:d}/{tw:d}/{tc:d}/{tr:d}/{tt:d} ',
+        '{status_code:d} ',
+        '{bytes_sent:d} ',
         '{captured_request_cookie} ',
         '{captured_response_cookie} ',
         '{country} ',
-        '{actconn}/{feconn}/{beconn}/{srv_conn}/{retries} ',
-        '{srv_queue}/{backend_queue} ',
+        '{actconn:d}/{feconn:d}/{beconn:d}/{srv_conn:d}/{retries:d} ',
+        '{srv_queue:d}/{backend_queue:d} ',
         '"{http_request}"',
     ]
 }
+def get_time_haproxy(string_time):
+    return parse.datetime.strptime(string_time, "%d/%b/%Y:%H:%M:%S.%f")
 
+    
+def get_time_ats(string_time):
 
+    string_time = string_time[:4] + " " + string_time[4:6] + " " + string_time[6:8] + " " + string_time[9:11] + " " +  string_time[12:14] + " " + string_time[15:17]
+    return parse.datetime.strptime(string_time, "%Y %m %d %H %M %S")
+
+    
 def detect_log_type(line):
+
     for TYPE in LOG_TYPES:
         log_format = "".join(LOG_TYPES[TYPE])
-        error_info = parse.parse(log_format, line)
+        if TYPE == 'HAPROXY' or TYPE == 'HAPROXY2':
+            error_info = parse.parse(log_format, line, dict(get_time_haproxy = get_time_haproxy))
+        elif TYPE == 'ATS':
+            error_info = parse.parse(log_format, line, dict(get_time_ats = get_time_ats))
+        else:
+            error_info = parse.parse(log_format, line)
 
         if error_info:
-            return log_format
+            return TYPE, log_format
 
-    raise "Unknown log type", line
+    raise Exception("Unknown log type \n" + line)
 
 
-def parse_log(line, log_format):
-    error_info = parse.parse(log_format, line)
+def parse_log(line, log_type, log_format):
+    
+    if log_type == 'HAPROXY' or log_type == 'HAPROXY2':
+        error_info = parse.parse(log_format, line, dict(get_time_haproxy = get_time_haproxy))
+    elif log_type == 'ATS':
+        error_info = parse.parse(log_format, line, dict(get_time_ats = get_time_ats))
+    else:
+        error_info = parse.parse(log_format, line)
 
     if error_info:
-        return error_info.named
+         return error_info.named
+    elif log_type == 'HAPROXY':
+        return parse_log(line, 'HAPROXY2', "".join(LOG_TYPES['HAPROXY2']))
+    elif log_type == 'HAPROXY2':
+        return parse_log(line, 'HAPROXY', "".join(LOG_TYPES['HAPROXY']))
     else:
-        raise "Unknown log type", line 
+        raise Exception("Unknown log type \n" + line)
