aff2c4746f90c0c262a634b21e6bbed2b40f9e06
Sam Hoang Van <samhv.ict@gmail.com>
2015-07-15 12:22:16 +0700

    Mod: times --> time, rewrite function get_time_ats

--- parser.py
+++ parser.py
@@ -1,95 +1,97 @@
-__author__ = 'lu-sa'
-
 import parse
 import raven
 
 
 LOG_TYPES = {
     'HAPROXY': [
-        '{ip}:{port:d} ',
-        '[{times:get_time_haproxy}] ',
-        '{frontend_name_transport} ',
-        '{backend_name}/{server_name} ',
-        '{tq:d}/{tw:d}/{tc:d}/{tr:d}/{tt:d} ',
-        '{status_code:d} ',
-        '{bytes_sent:d} ',
-        '{captured_request_cookie} ',
-        '{captured_response_cookie} ',
-        '{country} ',
-        '{actconn:d}/{feconn:d}/{beconn:d}/{srv_conn:d}/{retries:d} ',
-        '{srv_queue:d}/{backend_queue:d} ',
+        '{ip}:{port:d}',
+        '[{time:get_time_haproxy}]',
+        '{frontend_name_transport}',
+        '{backend_name}/{server_name}',
+        '{tq:d}/{tw:d}/{tc:d}/{tr:d}/{tt:d}',
+        '{status_code:d}',
+        '{bytes_sent:d}',
+        '{captured_request_cookie}',
+        '{captured_response_cookie}',
+        '{country}',
+        '{actconn:d}/{feconn:d}/{beconn:d}/{srv_conn:d}/{retries:d}',
+        '{srv_queue:d}/{backend_queue:d}',
         '{{{other}}} "{http_request}"',
     ],
 
     'LIGHTTPD': [
-        '{ip} ',
-        '[{time:th}] ',
-        '"{http_request}" ',
-        '{status_code:d} ',
-        '{byte_sent:d} ',
-        '"{referer}" ',
-        '"{user_agent}" ',
-        '{byte_in:d} ',
-        '{byte_out:d} ',
-        '{time_used:d} ',
+        '{ip}',
+        '[{time:th}]',
+        '"{http_request}"',
+        '{status_code:d}',
+        '{byte_sent:d}',
+        '"{referer}"',
+        '"{user_agent}"',
+        '{byte_in:d}',
+        '{byte_out:d}',
+        '{time_used:d}',
         '{request_hostname}',
     ],
 
 
     'NGINX': [
-        '{ip} ',
-        '{http_x_forwarded_for} ',
-        '[{time:ti}] ',
-        '{http_host} ',
-        '"{http_request}" ',
-        '{status_code:d} ',
-        '{bytes_sent:d} ',
-        '"{http_referer}" ',
+        '{ip}',
+        '{http_x_forwarded_for}',
+        '[{time:ti}]',
+        '{http_host}',
+        '"{http_request}"',
+        '{status_code:d}',
+        '{bytes_sent:d}',
+        '"{http_referer}"',
         '{http_user_agentt}',
     ],
 
     'ATS': [
-        '{time:get_time_ats} RESPONSE: sent ',
-        '{ip} status ',
-        '{status_code:d} ',
-        '({accelerator}) for ',
+        '{time:get_time_ats} RESPONSE: sent',
+        '{ip} status',
+        '{status_code:d}',
+        '({accelerator}) for',
         "'{http_referer}'",
     ],
 
     'HAPROXY2': [
-        '{ip}:{port:d} ',
-        '[{times:get_time_haproxy}] ',
-        '{frontend_name_transport} ',
-        '{backend_name}/{server_name} ',
-        '{tq:d}/{tw:d}/{tc:d}/{tr:d}/{tt:d} ',
-        '{status_code:d} ',
-        '{bytes_sent:d} ',
-        '{captured_request_cookie} ',
-        '{captured_response_cookie} ',
-        '{country} ',
-        '{actconn:d}/{feconn:d}/{beconn:d}/{srv_conn:d}/{retries:d} ',
-        '{srv_queue:d}/{backend_queue:d} ',
+        '{ip}:{port:d}',
+        '[{time:get_time_haproxy}]',
+        '{frontend_name_transport}',
+        '{backend_name}/{server_name}',
+        '{tq:d}/{tw:d}/{tc:d}/{tr:d}/{tt:d}',
+        '{status_code:d}',
+        '{bytes_sent:d}',
+        '{captured_request_cookie}',
+        '{captured_response_cookie}',
+        '{country}',
+        '{actconn:d}/{feconn:d}/{beconn:d}/{srv_conn:d}/{retries:d}',
+        '{srv_queue:d}/{backend_queue:d}',
         '"{http_request}"',
     ]
 }
+
+
 def get_time_haproxy(string_time):
+
     return parse.datetime.strptime(string_time, "%d/%b/%Y:%H:%M:%S.%f")
 
-    
+
 def get_time_ats(string_time):
 
-    string_time = string_time[:4] + " " + string_time[4:6] + " " + string_time[6:8] + " " + string_time[9:11] + " " +  string_time[12:14] + " " + string_time[15:17]
-    return parse.datetime.strptime(string_time, "%Y %m %d %H %M %S")
+    return parse.datetime.strptime(string_time, "%Y%m%d.%Hh%Mm%Ss")
 
-    
+
 def detect_log_type(line):
 
     for TYPE in LOG_TYPES:
-        log_format = "".join(LOG_TYPES[TYPE])
+        log_format = " ".join(LOG_TYPES[TYPE])
         if TYPE == 'HAPROXY' or TYPE == 'HAPROXY2':
-            error_info = parse.parse(log_format, line, dict(get_time_haproxy = get_time_haproxy))
+            error_info = parse.parse(log_format, line,
+                                     dict(get_time_haproxy=get_time_haproxy))
         elif TYPE == 'ATS':
-            error_info = parse.parse(log_format, line, dict(get_time_ats = get_time_ats))
+            error_info = parse.parse(log_format, line,
+                                     dict(get_time_ats=get_time_ats))
         else:
             error_info = parse.parse(log_format, line)
 
@@ -100,19 +102,21 @@ def detect_log_type(line):
 
 
 def parse_log(line, log_type, log_format):
-    
+
     if log_type == 'HAPROXY' or log_type == 'HAPROXY2':
-        error_info = parse.parse(log_format, line, dict(get_time_haproxy = get_time_haproxy))
+        error_info = parse.parse(log_format, line,
+                                 dict(get_time_haproxy=get_time_haproxy))
     elif log_type == 'ATS':
-        error_info = parse.parse(log_format, line, dict(get_time_ats = get_time_ats))
+        error_info = parse.parse(log_format, line,
+                                 dict(get_time_ats=get_time_ats))
     else:
         error_info = parse.parse(log_format, line)
 
     if error_info:
-         return error_info.named
+        return error_info.named
     elif log_type == 'HAPROXY':
-        return parse_log(line, 'HAPROXY2', "".join(LOG_TYPES['HAPROXY2']))
+        return parse_log(line, 'HAPROXY2', " ".join(LOG_TYPES['HAPROXY2']))
     elif log_type == 'HAPROXY2':
-        return parse_log(line, 'HAPROXY', "".join(LOG_TYPES['HAPROXY']))
+        return parse_log(line, 'HAPROXY', " ".join(LOG_TYPES['HAPROXY']))
     else:
         raise Exception("Unknown log type \n" + line)
